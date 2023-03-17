d1ff37d46b08436cc45a12ae6d76e22fc7bf3e3c
LyHoang2410 <ly.hoangdoan@gmail.com>
2023-03-17 17:15:20 +0700

    done

--- Condition2/BT-DoYouLoveMejs.js
+++
@@ -1,9 +0,0 @@
-function yesClick(){
-    alert("Yes, You are right. I love you too");
-}
-function noTouch(){
-    let x = Math.round(Math.random() * window.innerWidth);
-    let y = Math.round(Math.random() * window.innerHeight);
-    document.getElementById("cntno").style.left = x + 'px';
-    document.getElementById("cntno").style.top = y + 'px';
-}
